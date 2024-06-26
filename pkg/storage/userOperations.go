b2ef3fdcc1155e2aa70a10dab924c65904cf1cf3
lokeshllkumar <lokeshkumar2015.2004@gmail.com>
2024-06-26 13:15:12 +0530

    created functions to perform database operations

---
+++ pkg/storage/userOperations.go
@@ -0,0 +1,56 @@
+package storage
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/lokeshllkumar/auth-service/utils"
+)
+
+func (db *DB) AddUser(username string, password string) error {
+	addCmd := `INSERT INTO users (username, password) VALUES (?, ?)`
+	encPswd, err := utils.EncryptPassword(password)
+	if err != nil {
+		fmt.Printf("Error encrypting password\n")
+		return err
+	}
+	_, err = db.Conn.Exec(addCmd, username, encPswd)
+	if err != nil {
+		fmt.Printf("Error adding entry to database\n")
+		return err
+	}
+
+	return nil
+}
+
+func (db *DB) GetUser(username string) (User, error) {
+	var user User
+	getCmd := `SELECT username, password FROM users WHERE username = ?`
+	rows, err := db.Conn.Query(getCmd, username)
+	if err != nil {
+		fmt.Printf("Error retrieving user information\n")
+		panic(err)
+	}
+	err = rows.Scan(&user.Username, &user.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return user, err
+		}
+		fmt.Printf("Error parsing retrieved data\n")
+		panic(err)
+	}
+
+	return user, nil
+}
+
+func (db *DB) UpdateUser(username string, newPassword string) error {
+	encPswd, err := utils.EncryptPassword(newPassword)
+	if err != nil {
+		fmt.Printf("Error encypting password\n")
+		return err
+	}
+
+	updateCmd := `UPDATE users SET password = ? WHERE username = ?`
+	_, err = db.Conn.Exec(updateCmd, encPswd, username)
+	return err
+}

d2849c25ce33b98f7895fcc2db42c6d0154a751c
lokeshllkumar <lokeshkumar2015.2004@gmail.com>
2024-07-03 15:42:28 +0530

    implemented JWT authentication middleware

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -1,4 +1,37 @@
 package middleware
 
-import ()
+import (
+	"net/http"
+	"strings"
 
+	"github.com/gin-gonic/gin"
+	"github.com/lokeshllkumar/auth-service/pkg/auth"
+)
+
+func JWTMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Forbidden authorization header"})
+			c.Abort()
+			return
+		}
+
+		token := strings.TrimPrefix(authHeader, "Bearer")
+		if token == authHeader {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Malformed token"})
+			c.Abort()
+			return
+		}
+
+		claims, err := auth.ValidateToken(token)
+		if err != nil {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("Username", claims.Username)
+		c.Next()
+	}
+}

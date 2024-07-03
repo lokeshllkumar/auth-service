0edb750d84fd80923b1f1cf8d01862c756d12845
lokeshllkumar <lokeshkumar2015.2004@gmail.com>
2024-07-03 15:42:17 +0530

    created routes for login and callback functions

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import ()
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lokeshllkumar/auth-service/pkg/handlers"
+	"github.com/lokeshllkumar/auth-service/pkg/middleware"
+	"github.com/lokeshllkumar/auth-service/pkg/storage"
+)
 
 func main() {
-	
+	db, err := storage.NewStorage()
+	if err != nil {
+		log.Fatalf("Database connection failed: %v", err)
+	}
+
+	r := gin.Default()
+
+	r.POST("/login", handlers.LoginHandler(db))
+	r.GET("/oauth/login", handlers.OAuthLoginHandler())
+	r.GET("/callback", handlers.OAuthCallbackHandler(db))
+
+	authorized := r.Group("/")
+	authorized.Use(middleware.JWTMiddleware())
+	authorized.GET("/protected", func(c *gin.Context) {
+		username := c.GetString("Username")
+		c.JSON(http.StatusOK, gin.H{"message": "Welcome " + username + "!"})
+	})
 }
